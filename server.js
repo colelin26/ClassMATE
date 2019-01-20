4cf31c90f61eaf92f64a022e0e7f20488275da83
Cole Lin <cole.lin@hubdoc.com>
2019-01-20 16:34:22 -0500

    linked to the server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const studentRouter = require("./server/studyBuddy/student.router");
+const groupRouter = require("./server/studyBuddy/group.router");
 
 const app = express();
 const port = process.env.PORT || 8081;
-
+app.use("/student", studentRouter);
+app.use("/study", groupRouter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "build")));
